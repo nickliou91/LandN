bdeb261c8006e35cb329096183b18b110b86ae16
~NiCK~ <nickliou91@gmail.com>
2014-12-31 02:03:25 +0800

    before product model and database

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require owl.carousel
+//= require bootstrap
+
+
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+ 
+      // navigation : true, // Show next and prev buttons
+      autoPlay: 5000,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      transitionStyle:"fade"
+ 
+  });
+ 
+});
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def product
+  end
+
+  def about
+  end
+end
