eca5d071229765a7d1094248d0dc9045ae6a735b
~NiCK~ <nickliou91@gmail.com>
2015-01-12 03:31:52 +0800

    carrier_wave intializer modified the s3 access key

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
       :region                => 'ap-southeast-1'
     }
     config.fog_directory     =  ENV['S3_BUCKET_NAME']
